8f3c8ddddc2023aee6cac6a39ab6cbcb9fe4a487
3159275585@qq.com <3159275585@qq.com>
2017-06-02 13:59:39 +0800

    首次提交

--- README.md
+++ README.md
@@ -1,2 +1,122 @@
-# Marquee
-仿淘宝头条垂直滚动，垂直走马灯
+# Marquee-master
+仿淘宝首页的淘宝头条垂直滚动
+滚动布局继承了是ViewFlipper,可以实现进入动画，退出动画。切换时间间隔。
+MarqueeView可用于多行或单行滚动，滚动的布局可以自定义。
+
+### 效果图  
+<img src="/screenshots/marqueeview.gif" style="width: 30%;">
+
+
+### 使用介绍
+
+#### 1、添加依赖<br>
+##### Step 1. Add it in your root build.gradle at the end of repositories:
+
+```
+    allprojects {
+		repositories {
+			...
+			maven { url "https://jitpack.io" }
+		}
+	}
+```
+
+##### Step 2. Add the dependency
+
+```
+    dependencies {
+	          compile 'com.github.sun-sky:Marquee:1.0.0'
+	}
+```
+#### 2、在布局文件中添加MarqueeView，可以设置自定义属性<br>
+
+**xml文件**
+
+```
+ <com.sunsky.marqueeview.MarqueeView
+        android:id="@+id/upview1"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:layout_centerVertical="true"
+        android:padding="10dp"
+        app:animDuration="1000"
+        app:interval="5000"
+        app:isSetAlphaAnim="true" />
+```
+
+**Attributes:**
+
+|attr属性|description 描述|default vuale 默认值|
+|:---|:---|:---|
+|isSetAlphaAnim|是否显示淡入淡出动画|true|
+|interval|切换间隔时间|5000（5秒）|
+|animDuration|动画时间|2000（2秒）|
+
+#### 3、Activity中使用<br>
+
+**控件初始化**
+
+```java
+  marqueeView1 = (MarqueeView) findViewById(R.id.upview1);
+  initdata();
+  setViewSingleLine();
+  marqueeView1.setViews(views1);
+
+```
+
+**初始化数据**
+
+```java
+    private void initdata() {
+        data = new ArrayList<>();
+        data.add("git常用命令");
+        data.add("Git配置SSH访问GitHub(window)");
+        data.add("关于java的抽象和接口");
+        data.add("阿里HotFix2.0升级详解 畅谈热修复领域那些事");
+
+    }
+```
+
+
+**初始化view队列**
+
+```java
+    /**
+     * 自定义布局——单行滚动
+     */
+    private void setViewSingleLine() {
+        for (int i = 0; i < data.size(); i++) {
+            final int position = i;
+            //设置滚动的单个布局
+            LinearLayout moreView = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.item_view_single, null);
+            //初始化布局的控件
+            TextView tv1 = (TextView) moreView.findViewById(R.id.tv1);
+
+            /**
+             * 设置监听
+             */
+            moreView.findViewById(R.id.rl).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getApplicationContext(), position + "你点击了" + data.get(position).toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            //进行对控件赋值
+            tv1.setText(data.get(i).toString());
+
+            //添加到循环滚动数组里面去
+            views2.add(moreView);
+        }
+    }
+
+```
+
+
+
+# Thanks
+该项目参考了：
+
+* [https://github.com/dreamlivemeng/UpMarqueeTextView-master](https://github.com/dreamlivemeng/UpMarqueeTextView-master)
+
+#### ⊙开源不易，希望给个star或者fork奖励
---
+++ app/src/main/java/com/sunsky/marqueeviewdemo/MainActivity.java
@@ -0,0 +1,166 @@
+package com.sunsky.marqueeviewdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.dreamlive.upmarqueeviewdemo.R;
+import com.sunsky.marqueeview.MarqueeView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 类描述：仿淘宝首页的 淘宝头条滚动的自定义View
+ * 双行
+ * 创建人：sunsky
+ * 创建时间：2017/6/1 17:47
+ * 修改人：sunsky
+ * 修改时间：2017/6/1 17:47
+ * 修改备注：
+ */
+public class MainActivity extends AppCompatActivity {
+
+
+    private MarqueeView marqueeView1;
+    private MarqueeView marqueeView2;
+    List<String> data = new ArrayList<>();
+    List<View> views1 = new ArrayList<>();
+    List<View> views2 = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initParam();
+        initdata();
+        initView();
+    }
+
+    /**
+     * 实例化控件
+     */
+    private void initParam() {
+        marqueeView1 = (MarqueeView) findViewById(R.id.upview1);
+        marqueeView2 = (MarqueeView) findViewById(R.id.upview2);
+    }
+
+    /**
+     * 初始化界面程序
+     */
+    private void initView() {
+        setViewTwoLines();
+        setViewSingleLine();
+        marqueeView1.setViews(views1);
+        marqueeView2.setViews(views2);
+//        /**
+//         * 设置item_view的监听
+//         */
+//        marqueeView.setOnItemClickListener(new MarqueeView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(int position, View view) {
+//                Toast.makeText(MainActivity.this, "你点击了第几个items" + position, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+    }
+
+    /**
+     * 初始化需要循环的View
+     * 为了灵活的使用滚动的View，所以把滚动的内容让用户自定义
+     * 假如滚动的是三条或者一条，或者是其他，只需要把对应的布局，和这个方法稍微改改就可以了，
+     * 条数为奇数时做了些处理：奇数时最后一个没有内容 将第一个拼接到最后显示
+     */
+    private void setViewTwoLines() {
+        for (int i = 0; i < data.size(); i = i + 2) {
+            final int position = i;
+            //设置滚动的单个布局
+            LinearLayout moreView = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.item_view, null);
+            //初始化布局的控件
+            TextView tv1 = (TextView) moreView.findViewById(R.id.tv1);
+            TextView tv2 = (TextView) moreView.findViewById(R.id.tv2);
+
+            /**
+             * 设置监听
+             */
+            moreView.findViewById(R.id.rl).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(MainActivity.this, position + "你点击了" + data.get(position).toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            /**
+             * 设置监听
+             */
+            moreView.findViewById(R.id.rl2).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (data.size() > position + 1) {
+                        Toast.makeText(getApplicationContext(), position + "你点击了" + data.get(position + 1).toString(), Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), position + "你点击了" + data.get(0).toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+            //进行对控件赋值
+            tv1.setText(data.get(i).toString());
+            if (data.size() > i + 1) {
+                //因为淘宝那儿是两条数据，但是当数据是奇数时就不需要赋值第二个，所以加了一个判断，还应该把第二个布局给隐藏掉
+                tv2.setText(data.get(i + 1).toString());
+            } else {
+                //moreView.findViewById(R.id.rl2).setVisibility(View.GONE);
+                //修改了最后一个没有 将第一个拼接到最后显示
+                tv2.setText(data.get(0).toString());
+            }
+
+            //添加到循环滚动数组里面去
+            views1.add(moreView);
+        }
+    }
+
+
+    /**
+     * 自定义布局——单行滚动
+     */
+    private void setViewSingleLine() {
+        for (int i = 0; i < data.size(); i++) {
+            final int position = i;
+            //设置滚动的单个布局
+            LinearLayout moreView = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.item_view_single, null);
+            //初始化布局的控件
+            TextView tv1 = (TextView) moreView.findViewById(R.id.tv1);
+
+            /**
+             * 设置监听
+             */
+            moreView.findViewById(R.id.rl).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getApplicationContext(), position + "你点击了" + data.get(position).toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            //进行对控件赋值
+            tv1.setText(data.get(i).toString());
+
+            //添加到循环滚动数组里面去
+            views2.add(moreView);
+        }
+    }
+
+
+    /**
+     * 初始化数据
+     */
+    private void initdata() {
+        data = new ArrayList<>();
+        data.add("git常用命令");
+        data.add("Git配置SSH访问GitHub(window)");
+        data.add("关于java的抽象和接口");
+//        data.add("阿里HotFix2.0升级详解 畅谈热修复领域那些事");
+
+    }
+}
